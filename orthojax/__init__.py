8ffd368ddfba9c835e10b71df67da11a1cacc05f
Ilias Bilionis <ebilionis@gmail.com>
2024-01-15 09:01:49 -0500

    Updated version.

--- orthojax/__init__.py
+++ orthojax/__init__.py
@@ -4,7 +4,7 @@ orthjax.
 Orthogonal polynomials in jax.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = 'Ilias Bilionis'
 __credits__ = 'Purdue University, School of Mechanical Engineering'
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='orthojax',
-    version='0.1.0',    
+    version='0.1.1',    
     description='Orthogonal polynomials in Jax',
     long_description=long_description,
     long_description_content_type='text/markdown',
