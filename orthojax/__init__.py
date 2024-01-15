585776b48f253149178ef28349a405c68ab1c94d
Ilias Bilionis <ebilionis@gmail.com>
2024-01-15 08:09:11 -0500

    Added version and credits.

--- orthojax/__init__.py
+++ orthojax/__init__.py
@@ -1,1 +1,11 @@
+"""
+orthjax.
+
+Orthogonal polynomials in jax.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Ilias Bilionis'
+__credits__ = 'Purdue University, School of Mechanical Engineering'
+
 from .orthpol import *
